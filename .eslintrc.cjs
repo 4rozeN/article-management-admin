760620aacd3175d9db96b293a52b6ac21d3fe0cf
4rozeN <1686617971@qq.com>
2024-09-21 14:48:16 +0800

    -初始化了项目。 -配置了ESLint和prettier。 -配置了husky。

---
+++ .eslintrc.cjs
@@ -0,0 +1,35 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-prettier/skip-formatting'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    'prettier/prettier': [
+      'warn',
+      {
+        singleQuote: true, // 使用单引号
+        semi: false, // 结尾不使用分号
+        printWidth: 80, // 单行最大长度
+        trailingComma: 'none', // 结尾不使用逗号
+        endOfLine: 'auto' // 自动根据操作系统换行(win和mac不一致)
+      }
+    ],
+    'vue/multi-word-component-names': [
+      'warn',
+      {
+        ignores: ['index'] // 忽略 index.vue 文件名必须为多词的规则
+      }
+    ],
+    'vue/no-setup-props-destructure': ['off'], // 允许解构 setup 函数中的 props
+    // 添加未定义变量错误提示，create-vue@3.6.3 关闭
+    'no-undef': 'error'
+  }
+}
