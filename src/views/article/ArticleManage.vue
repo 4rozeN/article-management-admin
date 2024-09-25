4d926642853ae3f41ab145823d0e9358bf5bcce8
4rozeN <1686617971@qq.com>
2024-09-26 03:50:12 +0800

    -配置了新的组件忽略 -优化了文章管理页中未使用的参数

--- src/views/article/ArticleManage.vue
+++ src/views/article/ArticleManage.vue
@@ -1,12 +1,39 @@
 <script setup>
-import { Delete, Edit } from '@element-plus/icons-vue'
+import { Delete, Edit, Plus, Link } from '@element-plus/icons-vue'
 import { ref, onMounted } from 'vue'
-import { useArticleService, useArticleCategoryService } from '@/api/article'
+import { useUploadService } from '@/api/upload'
+import {
+  useArticleService,
+  useArticleCategoryService,
+  updateArticleByIdService,
+  deleteArticleByIdService,
+  addArticleService,
+  useArticleByConditionService
+} from '@/api/article'
 import dayjs from 'dayjs' // 导入dayjs格式化日期
+import { QuillEditor } from '@vueup/vue-quill'
+import '@vueup/vue-quill/dist/vue-quill.snow.css'
+import { useArticleStore } from '@/stores'
 
+const useStore = useArticleStore()
+const baseUploadUrl = import.meta.env.VITE_BASE_UPLOAD_URL
 const articleList = ref([])
 const articleCategoryList = ref([])
-
+const formModel = ref({
+  id: '',
+  title: '',
+  content: '',
+  cover_img: '',
+  state: false,
+  article_category: 3 // 接收分类的id，该id非documentId而是真实id，用于接口提交
+})
+const oprationType = ref('')
+const showDrawer = ref(false)
+const direction = ref('rtl')
+const loading = ref(false)
+const form = ref(null) // 定义form用于接收模板引用进行表单提交预校验
+const srcList = ref([]) // 定义图片预览列表
+// 获取文章列表
 const getArticleList = async () => {
   const res = await useArticleService()
   articleList.value = res.data.data
@@ -18,31 +45,330 @@ const getArticleList = async () => {
   articleList.value.forEach((item) => {
     item.state = item.state ? '已发布' : '草稿'
   })
+  // 将文章列表遍历，将封面图列表赋值给srcList
+  srcList.value = articleList.value.map((item) => {
+    if (item.cover_img) {
+      return baseUploadUrl + item.cover_img.url
+    } else {
+      return ''
+    }
+  })
+  console.log('articleList.value', articleList.value)
+  console.log('srcList', srcList.value)
 }
+// 获取分类列表
 const getArticleCategoryList = async () => {
   const res = await useArticleCategoryService()
   articleCategoryList.value = res.data.data
-  console.log('getArticleCategoryList', articleCategoryList.value)
+  // console.log('getArticleCategoryList', articleCategoryList.value)
 }
 onMounted(async () => {
   await getArticleList()
   await getArticleCategoryList()
 })
 
+// 定义条件查询的项目
 const categoryRow = ref('')
 const stateRow = ref('')
-const onEditArticle = (row) => {
-  console.log(row)
+// 条件查询的响应事件
+const handleQuery = async () => {
+  console.log('条件查询：', categoryRow.value, stateRow.value)
+  // 构建条件对象
+  const conditionObj = {
+    id: categoryRow.value,
+    state: stateRow.value
+  }
+  // 调用接口
+  await useArticleByConditionService(conditionObj)
+    .then((res) => {
+      articleList.value = res.data.data
+      // 格式化日期
+      articleList.value.forEach((item) => {
+        item.createdAt = dayjs(item.createdAt).format('YYYY-MM-DD HH:mm:ss')
+      })
+      // 处理state显示，true为已发布；false为草稿
+      articleList.value.forEach((item) => {
+        item.state = item.state ? '已发布' : '草稿'
+      })
+    })
+    .catch((err) => {
+      ElMessage.error('条件查询失败：' + err)
+    })
 }
+
+// 条件查询重置按钮的响应事件
+const handleReset = () => {
+  categoryRow.value = ''
+  stateRow.value = ''
+  // 重新拉取文章列表
+  getArticleList()
+  ElNotification({
+    title: 'Success',
+    message: '重置成功',
+    type: 'success',
+    position: 'bottom-left'
+  })
+}
+
+// 编辑文章的响应事件
+const onEditArticle = (row) => {
+  console.log('row', row)
+  // 设置操作类型
+  oprationType.value = 'edit'
+  // 将文章数据渲染到表格上
+  formModel.value = {
+    id: row.documentId,
+    title: row.title,
+    content: row.content,
+    state: row.state,
+    article_category: row.article_category.id // 该id非documentId而是真实id
+  }
+  if (!row.cover_img) {
+    formModel.value.cover_img = ''
+    // 设置预览图
+    preImgUrl.value = ''
+  } else {
+    ;(formModel.value.cover_img = row.cover_img.id),
+      // 设置预览图
+      (preImgUrl.value = baseUploadUrl + row.cover_img.url)
+  }
+  // 打开弹层
+  showDrawer.value = true
+  // console.log('弹层被触发，表单内容为：', formModel.value)
+}
+// 删除文章的响应事件
 const onDeleteArticle = (row) => {
   console.log(row)
+  deleteArticleByIdService(row.documentId)
+  ElMessageBox.confirm('确定要删除该文章吗？', '提示', {
+    confirmButtonText: '确定',
+    cancelButtonText: '取消',
+    type: 'warning'
+  })
+  // 重新拉取文章列表
+  getArticleList()
+}
+
+const rules = {
+  title: [
+    { required: true, message: '请输入文章标题', trigger: 'blur' },
+    { min: 1, max: 20, message: '文章标题长度在 1 到 20 个字符', trigger: 'blur' }
+  ],
+  content: [
+    { required: true, message: '请输入文章内容', trigger: 'blur' },
+    { min: 1, max: 5000, message: '文章内容长度在 1 到 5000 个字符', trigger: 'blur' }
+  ],
+  cover_img: [{ required: true, message: '请上传文章封面图', trigger: 'blur' }],
+  state: [{ required: true, message: '请选择文章状态', trigger: 'change' }],
+  article_category: [{ required: true, message: '请选择文章分类', trigger: 'change' }]
+}
+const handleClose = () => {
+  ElMessageBox.confirm('您确定要关闭此弹层吗？未保存的修改可能丢失。', '提示', {
+    confirmButtonText: '确定',
+    cancelButtonText: '取消',
+    type: 'warning'
+  })
+    .then(() => {
+      handleCancel()
+    })
+    .catch(() => {
+      ElMessage.info('操作取消')
+    })
+}
+const handleCancel = () => {
+  // 展示加载中效果
+  loading.value = true
+  // 清空表单
+  formModel.value = {
+    id: '',
+    title: '',
+    content: '',
+    cover_img: '',
+    state: false,
+    article_category: 0 // 该id非documentId而是真实id
+  }
+  // 重置操作类型
+  oprationType.value = ''
+  // 关闭弹层
+  showDrawer.value = false
+  // 关闭加载中效果
+  loading.value = false
+}
+
+// 预览图片的URL
+const preImgUrl = ref('')
+const beforeImgUpload = (rawFile) => {
+  // 定义一个可以上传的图片类型数组，进行白名单筛选
+  const allowTypes = ['image/jpeg', 'image/png']
+  // 进行类型和大小校验
+  if (rawFile.type !== '' && !allowTypes.includes(rawFile.type)) {
+    ElMessageBox.confirm('您上传的图片格式不正确！', '提示', {
+      confirmButtonText: '确定',
+      cancelButtonText: '取消',
+      type: 'warning'
+    })
+    return false
+  } else if (rawFile.size / 1024 / 1024 > 2) {
+    ElMessage.error('图片大小不能超过 2MB!')
+    return false
+  }
+  // 返回false，阻止文件上传
+  return false
+}
+// 选择文件后触发的事件
+const onSelectFIle = (file) => {
+  console.log('选择了文件：', file)
+  // 生成本地预览 URL
+  preImgUrl.value = URL.createObjectURL(file.raw)
+  // 存入表单
+  formModel.value.cover_img = file.raw
+}
+
+// 移除文件后触发的事件
+const handleRemove = () => {
+  // 清空预览
+  preImgUrl.value = ''
+}
+
+// 新增文章的响应事件
+const onAddArticle = () => {
+  // 设置操作类型
+  oprationType.value = 'add'
+  // 查询store是否有草稿存在
+  const draft = useStore.getArticleDraft()
+  console.log(draft)
+  if (draft) {
+    // 存在草稿，将草稿内容渲染到表单上
+    formModel.value = {
+      title: draft.title,
+      content: draft.content,
+      cover_img: draft.cover_img,
+      state: false,
+      article_category: draft.article_category.id // 该id非documentId而是真实id
+    }
+    // 设置预览图
+    preImgUrl.value = baseUploadUrl + draft.cover_img
+  } else {
+    // 没有草稿直接重置表单
+    formModel.value = {
+      title: '',
+      content: '',
+      cover_img: '',
+      state: false,
+      article_category: null // 该id非documentId而是真实id
+    }
+    // 设置预览图
+    preImgUrl.value = ''
+  }
+  // 打开弹层
+  showDrawer.value = true
+}
+
+// 走提交的响应事件
+const handleSubmit = async () => {
+  // 展示加载中效果
+  loading.value = true
+  // 表单校验
+  await form.value.validate()
+  // 打印表单数据
+  console.log('表单数据：', formModel.value)
+  // 判断是否有封面图
+  if (preImgUrl.value) {
+    // 判断是否是url地址，如果是，则说明没有被更改过，直接提交无需上传
+    if (!preImgUrl.value.startsWith('http')) {
+      console.log('提交-当前封面被更换为本地图片')
+      // 上传封面图
+      const uploadRes = await useUploadService(formModel.value.cover_img)
+      // 将上传后的图片id赋值给formModel.value.cover_img
+      console.log('上传后的图片：', uploadRes)
+      formModel.value.cover_img = uploadRes.data[0].id
+    } else {
+      console.log('没有更改封面图')
+      // 说明没有被更改过，将表单中的cover_img删除掉
+      delete formModel.value.cover_img
+    }
+  }
+
+  // 走发布，state为true
+  formModel.value.state = true
+  // 判断操作类型
+  if (oprationType.value === 'add') {
+    // 新增文章
+    console.log('提交表单数据：', formModel.value)
+    await addArticleService(formModel.value)
+    ElMessage.success('文章发布成功！')
+  } else if (oprationType.value === 'edit') {
+    console.log('提交表单数据：', formModel.value)
+    await updateArticleByIdService(formModel.value.id, formModel.value)
+    ElMessage.success('文章修改成功！')
+  } else {
+    // 其他操作类型
+    ElMessage.error('当前操作异常！')
+  }
+  // 关闭弹层
+  showDrawer.value = false
+  // 重置表单
+  formModel.value = {
+    id: '',
+    title: '',
+    content: '',
+    cover_img: '',
+    state: false,
+    article_category: 0 // 该id非documentId而是真实id
+  }
+  // 重置操作类型
+  oprationType.value = ''
+  // 关闭加载中效果
+  loading.value = false
+  // 重新拉取文章列表
+  await getArticleList()
+  // 给予提示
+  ElNotification({
+    title: 'Success',
+    message: '文章列表更新成功！',
+    type: 'success',
+    position: 'bottom-left'
+  })
+}
+// 走保存草稿的响应事件
+const handleSaveDraft = () => {
+  // 获取表单数据
+  console.log('保存草稿：', formModel.value)
+  useStore.setArticleDraft(formModel.value)
+  // 关闭弹层
+  showDrawer.value = false
+  // 给予提示
+  ElNotification({
+    title: 'Success',
+    message: '文章已保存为草稿！',
+    type: 'info',
+    position: 'bottom-left'
+  })
+}
+// 清除草稿的响应事件
+const handleClearDraft = () => {
+  // 展示加载中效果
+  loading.value = true
+  // 调用store中的方法清除草稿
+  useStore.clearArticleDraft()
+  // 关闭弹层
+  showDrawer.value = false
+  // 关闭加载中效果
+  loading.value = false
+  // 给予提示
+  ElNotification({
+    title: 'Success',
+    message: '草稿已清除！',
+    type: 'info',
+    position: 'bottom-left'
+  })
 }
 </script>
 
 <template>
   <page-container title="文章管理">
     <template #extra>
-      <el-button type="primary">发布文章</el-button>
+      <el-button type="primary" @click="onAddArticle()">发布文章</el-button>
     </template>
     <!-- 定义表头操作项 -->
     <el-form inline>
@@ -73,19 +399,51 @@ const onDeleteArticle = (row) => {
         </el-select>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary">查询</el-button>
-        <el-button>重置</el-button>
+        <el-button type="primary" @click="handleQuery()">查询</el-button>
+        <el-button @click="handleReset()">重置</el-button>
       </el-form-item>
     </el-form>
     <!-- 定义主体表格内容 -->
-    <el-table :data="articleList" style="width: 100%">
+    <el-table v-loading="loading" :data="articleList" style="width: 100%">
       <el-table-column label="文章标题" width="400">
         <template #default="{ row }">
-          <el-link type="primary" :underline="false">{{ row.title }}</el-link>
+          <el-link type="primary" :underline="false" @click="$router.push('/article/preview/${id}')"
+            ><el-icon> <Link /> </el-icon>{{ row.title }}</el-link
+          >
+        </template>
+      </el-table-column>
+      <el-table-column label="封面" width="180">
+        <template #default="scope">
+          <div style="display: flex; align-items: center">
+            <!-- scope.row.cover_img.url 是文章中的封面图片 URL -->
+            <el-image
+              :src="`${baseUploadUrl}${scope.row.cover_img.url}`"
+              :preview-src-list="[`${baseUploadUrl}${scope.row.cover_img.url}`]"
+              style="width: 100px; height: auto; cursor: pointer"
+              fit="cover"
+              @click.stop
+              :preview-teleported="true"
+              :lazy="true"
+            >
+              <template #error>
+                <div
+                  style="
+                    width: 100px;
+                    height: 100px;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                  "
+                >
+                  <span>加载失败</span>
+                </div>
+              </template>
+            </el-image>
+          </div>
         </template>
       </el-table-column>
       <el-table-column label="分类" prop="article_category.category_name"></el-table-column>
-      <el-table-column label="发表时间" prop="createdAt"> </el-table-column>
+      <el-table-column label="发表时间" prop="createdAt"></el-table-column>
       <el-table-column label="状态" prop="state"></el-table-column>
       <el-table-column label="操作" width="100">
         <template #default="{ row }">
@@ -109,7 +467,123 @@ const onDeleteArticle = (row) => {
         <el-empty description="没有数据" />
       </template>
     </el-table>
+    <!-- 定义弹层 -->
+    <el-drawer
+      v-model="showDrawer"
+      :direction="direction"
+      :lock-scroll="true"
+      :before-close="handleClose"
+      :size="'50%'"
+    >
+      <template #header>
+        <h4>当前操作为：{{ formModel.title ? '编辑或预览文章' : '新增文章' }}</h4>
+      </template>
+      <template #default>
+        <!-- 定义内容 -->
+        <div class="content">
+          <el-form
+            ref="form"
+            :model="formModel"
+            :rules="rules"
+            label-width="100px"
+            style="padding-right: 30px"
+          >
+            <el-form-item label="标题" prop="title">
+              <el-input v-model="formModel.title" minlength="1" maxlength="10"></el-input>
+            </el-form-item>
+            <el-form-item label="文章分类" prop="article_category">
+              <el-select
+                v-model="formModel.article_category"
+                placeholder="请选择文章分类"
+                size="default"
+                style="width: 100%"
+              >
+                <el-option
+                  v-for="item in articleCategoryList"
+                  :key="item.id"
+                  :label="item.category_name"
+                  :value="item.id"
+                >
+                </el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item label="文章封面图" prop="cover_img">
+              <el-upload
+                class="cover-img-uploader"
+                :auto-upload="false"
+                :before-upload="beforeImgUpload"
+                :on-remove="handleRemove"
+                :on-change="onSelectFIle"
+              >
+                <img v-if="preImgUrl" :src="preImgUrl" class="cover-img" />
+                <el-icon v-else class="cover-img-icon">
+                  <Plus />
+                </el-icon>
+              </el-upload>
+            </el-form-item>
+            <el-form-item label="文章内容" prop="content">
+              <div class="editor">
+                <quill-editor
+                  theme="snow"
+                  v-model:content="formModel.content"
+                  toolbar="full"
+                  contentType="html"
+                ></quill-editor>
+              </div>
+            </el-form-item>
+          </el-form>
+        </div>
+      </template>
+      <!-- 定义footer -->
+      <template #footer>
+        <div style="flex: auto">
+          <el-button v-if="oprationType === 'add'" type="default" @click="handleClearDraft()"
+            >清除草稿</el-button
+          >
+          <el-button v-if="oprationType === 'add'" type="default" @click="handleSaveDraft()"
+            >保存草稿</el-button
+          >
+          <el-button type="primary" @click="handleSubmit()">发布文章</el-button>
+        </div>
+      </template>
+    </el-drawer>
   </page-container>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.cover-img-uploader {
+  :deep() {
+    .cover-img {
+      width: 178px;
+      height: 178px;
+      display: block;
+    }
+
+    .el-upload {
+      border: 1px dashed #d9d9d9;
+      border-radius: 6px;
+      cursor: pointer;
+      position: relative;
+      overflow: hidden;
+      transition: var(--el-transition-duration-fast);
+    }
+
+    .el-upload:hover {
+      border-color: #409eff;
+    }
+
+    .el-icon.cover-img-icon {
+      font-size: 28px;
+      color: #8c939d;
+      width: 178px;
+      height: 178px;
+      text-align: center;
+    }
+  }
+}
+
+.editor {
+  width: 100%;
+  height: 300px;
+}
+</style>
